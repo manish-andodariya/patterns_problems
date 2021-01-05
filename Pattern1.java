7d7cde500ef5374688274075d057265f0487368e
Manish Andodariya <prajaptimanish07@gmail.com>
2021-01-05 10:47:52 +0530

    updated Pattern1 with another pattern

--- Pattern1.java
+++ Pattern1.java
@@ -1,6 +1,17 @@
 public class Pattern1 {
     public static void main(String[] args) {
+    	System.out.println('Pattern 1');
         for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println('Pattern 2');//new updated pattern
+         for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
